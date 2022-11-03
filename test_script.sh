b94d6b0f529dc51333b580920eff6171177020c5
sphiffy <aitzazdx@yahoo.com>
2022-11-03 12:02:59 -0400

    Version 6 from cloned repo

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Version 5: Re:Draft
+#Version 6: Re:Draft
 
 echo "Reverting back to draft as new code is needed"
 echo "additional lines"
+echo "This is an edit from a different cloned repo to the same remote repo"
